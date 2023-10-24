e8174d4f1268e633ca45a74ef9bd0f5b771bfdef
alius <alius@LAPTOP-NNR66SG1>
2023-10-24 15:07:19 +0200

    added css file and change the code in main file

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 import streamlit as st
-import plotly.express as px
-import requests
-import pandas as pd
-def fetch_data(api_url):
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        st.error("Failed to fetch data from API")
-        return None
 
-# This is first streamlit demo
-def main():
-    st.title("Dashboard with Streamlit")
-   
-    # Sidebar for user input
-    st.sidebar.header("User Input")
-    api_url = st.sidebar.text_input("API Endpoint URL")
+with open('styles.css', 'r') as css_file:
+    css_text = css_file.read()
+    st.markdown(f'<style>{css_text}</style>', unsafe_allow_html=True)
+# Define your list of navigation items
+st.sidebar.title("Navigation")
+navigation_items = st.sidebar.selectbox('Utbildning',["Behörighet yrkesprogram", "Examinerade gymnasieelev", "Förskolebarn", "Gymnasieutbildad", "Etablerade ungdomar"])
 
-    if api_url:
-        # Fetch data
-        data = fetch_data(api_url)
+# Apply custom CSS to style the navigation list
+st.markdown(f'<h3 class="nav-list">{navigation_items}</h3>', unsafe_allow_html=True)
 
-        if data:
-            # Convert data to a Pandas DataFrame
-            df =pd.DataFrame(data)
-            df_ar= pd.json_normalize(df['data'])
-            st.subheader("Data from API")
-            st.write(df_ar)
-            # Create plots using Plotly Express
-            st.subheader("Data Visualization")
-            fig = px.bar(df_ar, x='ar', y=['utbildade_man', 'utbildade_kvinnor','utbildade_totalt'], title='Plot')
-            
-            st.plotly_chart(fig)
-        else:
-            st.warning("No data to display.")
-
-if __name__ == "__main__":
-    main()
+# Create a list element for each navigation item
+navigation_items = st.sidebar.selectbox('Soc',["Hemtjänst äldreomsorg behandling", "Hemtjänst äldreomsorg åsikter", "Hemtjänst äldreomsorg trygghet", "Särskilt Äldreomsorg Behandling", "Särskilt äldreomsorg åsikter", "Särskilt äldreomsorg trygghet"])
